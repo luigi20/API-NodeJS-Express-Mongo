ef3d5090861bb4c7e8ad9a41e6f665b103e24376
Luis <luisopentec@gmail.com>
2021-10-18 16:12:17 -0300

    Token validation with middlewares and projectController creation

---
+++ src/controllers/projectController.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/auth.js');
+
+router.use(authMiddleware);
+router.get('/', (req, res) => {
+    res.send({ ok: true, id: req.userId })
+})
+
+module.exports = app => app.use('/projects', router)
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: false }))
 
 require('./controllers/authController')(app);
 
+require('./controllers/projectController')(app);
 
 
 app.listen(3000);
